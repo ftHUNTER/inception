aec897fa1a4f18d151ba56dd359c42cd3d75ccc8
reda <rchmouk@student.1337.ma>
2024-05-24 17:55:07 +0100

    now

--- inception/all_this_shit_in_one/creat.sh
+++ inception/all_this_shit_in_one/creat.sh
@@ -2,19 +2,19 @@
 
 service mariadb start
 
-sleep 5
-
-mysql --skip-password -u root -e "CREATE DATABASE IF NOT EXISTS redaDb;"
 
 mysql -u root -p --skip-password <<EOF
+CREATE DATABASE redaDb;
 CREATE USER 'new_user'@'localhost' IDENTIFIED BY 'new_password';
 GRANT ALL PRIVILEGES ON redaDb.* TO 'new_user'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-sleep 5
 
 service mariadb stop
 
+service nginx start
+
+service php7.4-fpm start
 
 mysqld_safe
